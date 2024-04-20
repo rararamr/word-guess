137d82d6ba9ebbc4d99e5230b22f88c64fda8842
gurii <167276760+gurifisuu@users.noreply.github.com>
2024-04-19 23:11:32 -0700

    Add files via upload

---
+++ words.js
@@ -0,0 +1,42 @@
+let words = [
+    {
+        word: "DUCK",
+        hint: "Its an animal that quacks and swims in water"
+    },
+    {
+        word: "PENCIL",
+        hint: "You use it to write and draw"
+    },
+    {
+        word: "BOOK",
+        hint: "A bunch of pages that you can read to learn new things"
+    },
+    {
+        word: "BIRD",
+        hint: "An animal that has feathers, wings, and usually flies in the sky"
+    },
+    {
+        word: "BALL",
+        hint: "A round object used in sports and games"
+    },
+    {
+        word: "HAT",
+        hint: "You wear it on your head to keep the sun out"
+    },
+    {
+        word: "DOG",
+        hint: "A furry friend who loves to play fetch"
+    },
+    {
+        word: "CRAYONS",
+        hint: "Something you use to color on paper"
+    },
+    {
+        word: "CAT",
+        hint: "A fluffy animal that says meow"
+    },
+    {
+        word: "HOTDOG",
+        hint: "You eat it with ketchup and mustard between two pieces of bread."
+    },
+]
